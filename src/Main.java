8599215cc9bace0f41c7750bf75f91e0589cb392
Polina Krupenina <krupenina.p@mail.ru>
2018-11-05 19:09:01 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        Summator newSum= new Summator();
+
+        newSum.setFirstNumber(2_000_000_000);
+        newSum.setSecondNumber(2_000_000_000L);
+
+        long summa = newSum.sum();
+        System.out.println(summa);
+
+    }
+}
---
+++ src/Summator.java
@@ -0,0 +1,24 @@
+public class Summator {
+
+
+    private long firstNumber;
+    private long secondNumber;
+
+    public void setFirstNumber(long a) {
+        firstNumber = a;
+    }
+    public void setSecondNumber(long a) {
+        secondNumber = a;
+    }
+    public long sum() {
+        return firstNumber + secondNumber;
+    }
+}
+
+
+
+
+
+
+
+
